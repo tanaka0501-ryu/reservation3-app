92e74d28b0709829c253f020890d791132068807
TANAKA <taexa0501@gmail.com>
2020-02-23 07:42:36 +0900

    商品明細変更

--- src/app/product/product-listings/product-listings.component.ts
+++ src/app/product/product-listings/product-listings.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.scss']
+  templateUrl: './product-listings.component.html',
+  styleUrls: ['./product-listings.component.scss']
 })
 export class ProductListComponent implements OnInit {
 
